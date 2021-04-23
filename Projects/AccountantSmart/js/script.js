c754c90e518e93a6e8dcd604fe0e4ddc79d9dc58
ismaelmarin2010 <ismaelmarin2010@gmail.com>
2021-04-23 20:06:46 -0300

    new repository

---
+++ Projects/AccountantSmart/js/script.js
@@ -0,0 +1,6 @@
+var lastUpdated = document.lastModified;
+document.getElementById("date").innerHTML = lastUpdated;
+
+var y = new Date();
+var fullYear = y.getFullYear();
+document.getElementById("year").innerHTML = fullYear;
